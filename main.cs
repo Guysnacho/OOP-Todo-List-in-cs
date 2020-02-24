72b23000c20491373ceba02dc19bc083fca474ce
Samuel Adetunji <guysnacho2@gmail.com>
2020-02-24 22:01:57 +0000

    Created an item class w/constructor

--- main.cs
+++ main.cs
@@ -1,5 +1,34 @@
 using System;
 
+//Item class - handles finer attributes like if an item has been checked off or not
+class item{
+	private bool complete;
+	private string toDo;
+
+	//Items aren't done by default for obvious reasons;
+	public item(string details){
+		complete = false;
+		toDo = details;
+	}
+
+	//I'm going to group the set and get methods
+	private string getItem(){
+		return toDo;
+	}
+
+	private bool isComplete(){
+		return complete;
+	}
+	
+	//Mutators
+	private void setItem(){
+		return toDo;
+	}
+
+	private void setCompletion(){
+		return complete;
+	}
+}
 //Todo List class - handles the adding, removing, and checking off of items
 class TodoList {
 
