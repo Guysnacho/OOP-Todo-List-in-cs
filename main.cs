614b666ab987854f15e2fcd40f97d9d6048a5a51
Samuel Adetunji <guysnacho2@gmail.com>
2020-02-24 21:51:01 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,15 @@
+using System;
+
+//Todo List class - handles the adding, removing, and checking off of items
+class TodoList {
+
+	public void addItem(){
+
+	}
+}
+
+class MainClass {
+  public static void Main (string[] args) {
+    Console.WriteLine ("Hello World");
+  }
+}
