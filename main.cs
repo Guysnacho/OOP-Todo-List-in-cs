aca51e481bec7514a704c834316ca8c7a256db5e
Samuel Adetunji <guysnacho2@gmail.com>
2020-02-24 22:31:59 +0000

    Added "add_item" functionality

--- main.cs
+++ main.cs
@@ -1,5 +1,5 @@
 using System;
-
+using System.Collections.Generic;
 //Item class - handles finer attributes like if an item has been checked off or not
 class item{
 	private bool complete;
@@ -37,15 +37,26 @@ class item{
 
 //Todo List class - handles the adding, removing, and checking off of items
 class toDoList {
-	List<item> toDo;
+	//I decided that the todo list would literally be a list.
+	// Doesn't make sense to make it more complex than it needs to be
+	List<item> toDo = new List<item>();
 
 	public toDoList(){
 		toDo = new List<>();
 	}
 
-	private void addItem(){
-
+	private void addItem(string details){
+		item current = new item(details);
+		toDo.Add(current);
 	}
+	//A default add item method
+	private void addItem(){
+		item current = new item();
+		toDo.Add(current);
+	}
+
+	//the remove method might give me trouble, maybe a hashmap would've been easier?
+	//Or a linkedlist
 }
 
 class MainClass {
